13f84ebcfe73803d1f12815f5487c7059bc42fde
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-07 23:46:23 -0400

    Update server.js

--- game/server.js
+++ game/server.js
@@ -57,7 +57,7 @@ app.post('/scores', (req, res) => {
 
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on https://duckduckgooose.onrender.com:${PORT}`);
 });
